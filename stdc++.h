2ad6b51643414abac2cec300f1df4ceee854c212
Rajatbir Singh Bajwa <rajatb088@gmail.com>
2016-08-30 22:37:04 +0530

    Update stdc++.h

--- stdc++.h
+++ stdc++.h
@@ -3,7 +3,7 @@
 1.  #include <bits/stdc++.h>
 
     It is basically a header file that also includes every standard library and stl include file. 
-    It's an implementation for precompiled headers.
+    It is an implementation for precompiled headers.
 
 2   ios_base::sync_with_stdio(false);
 
